ff8bb09940ec94ad269bda9dcf0803794b8c9b61
BarWieleba <barwieleba@gmail.com>
2022-05-03 13:10:19 +0200

    soap

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,12 @@ module com.example.tablesku {
     requires jakarta.xml.bind;
     requires jakarta.activation;
     requires com.fasterxml.jackson.databind;
+    requires jakarta.xml.ws;
+    requires jakarta.xml.soap;
 
 
     opens com.example.tablesku to javafx.fxml;
     opens com.example.tablesku.entity to javafx.base, jakarta.xml.bind, com.fasterxml.jackson.databind;
+    opens com.example.tablesku.soap to jakarta.xml.ws, jakarta.xml.soap;
     exports com.example.tablesku;
 }
