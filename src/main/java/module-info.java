3b28a5b878a6f9ea028097e5792c4834e3363ca8
BarWieleba <barwieleba@gmail.com>
2022-08-31 15:05:21 +0200

    jaxb

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,9 +7,10 @@ module com.example.tablesku {
     requires jakarta.xml.ws;
     requires jakarta.xml.soap;
     requires ini4j;
+    requires java.xml.bind;
 
 
-    opens com.example.tablesku to javafx.fxml;
+    opens com.example.tablesku to javafx.fxml, jakarta.xml.bind, org.glassfish.jaxb.runtime, org.glassfish.external.management.api, org.glassfish.ha.api;
     opens com.example.tablesku.entity to javafx.base, jakarta.xml.bind, com.fasterxml.jackson.databind;
     opens com.example.tablesku.allegroapi.entity to com.fasterxml.jackson.databind;
     exports com.example.tablesku;
